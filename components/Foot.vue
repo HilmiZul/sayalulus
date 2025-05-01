4c8758cd8200bd9ebc5082ed0c86cc8d8919477c
hilmizul <netspytux@gmail.com>
2025-05-01 19:23:01 +0700

    some update

--- components/Foot.vue
+++ components/Foot.vue
@@ -2,7 +2,7 @@
   <div class="row justify-content-center mt-5 mb-5">
     <div class="col-md-4">
       <footer class="card text-center small p-2 bg-shadow bg-light border-0">
-        &copy; 2023 - {{ tahun }}. Dibuat dengan ♥ oleh PPLG.
+        &copy; 2023 - {{ tahun }}. Dibuat dengan coding oleh PPLG.
       </footer>
     </div>
   </div>
