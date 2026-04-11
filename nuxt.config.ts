e982ee378dd70a3616f763f7c47b9538862730d5
hilmizul <netspytux@gmail.com>
2026-04-11 14:55:46 +0700

    add runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+import pkg from './package.json'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-04-29',
@@ -18,6 +19,12 @@ export default defineNuxtConfig({
       script: [{
         src: "https://cdn.jsdelivr.net/npm/bootstrap@5.2/dist/js/bootstrap.bundle.min.js"
       }]
+    }
+  },
+  runtimeConfig: {
+    public: {
+      appVersion: pkg.version,
+      author: pkg.author,
     }
   }
 })
