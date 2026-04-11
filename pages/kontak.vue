d5d4b3591d2091a851bc0cfa9f6483901b190e38
hilmizul <netspytux@gmail.com>
2026-04-11 21:11:05 +0700

    load from setting

--- pages/kontak.vue
+++ pages/kontak.vue
@@ -4,12 +4,11 @@
       <div class="text-center fs-3 prototype mb-4">
         Kontak.
       </div>
-      <div class="card bg-shadow p-4 border-0 small">
-        Jl. Depok, Kel. Sukamenak, Kec. Purbaratu <br/>
-        Kota Tasikmalaya 46196 <br />
-        Jawa Barat, Indonesia <br />
-        +62265312059 <br />
-        info@smkn4-tsm.sch.id
+      <div class="card bg-shadow p-4 border-0 white-space">
+        <span v-if="isLoading" class="card text-center bg-shadow border-0"><i class="bi bi-hourglass-split"></i> Sedang memuat</span>
+        <span v-else class="small">
+          {{ setting?.alamat_sekolah }}
+        </span>
       </div>
     </div>
   </div>
@@ -17,4 +16,26 @@
 
 <script setup>
 useHead({ title: "Kontak / Info Kelulusan SMKN 4 Tasikmalaya" })
+let client = useSupabaseClient()
+let setting = ref()
+let isLoading = ref(true)
+
+async function getSetting() {
+  isLoading.value = true
+  let { data, error } = await client.from('kelulusan_setting').select().eq('id', 1).single()
+  if(data) {
+    setting.value = data
+    isLoading.value = false
+  }
+}
+
+onMounted(() => {
+  getSetting()
+})
 </script>
+
+<style scoped>
+.white-space {
+  white-space: pre-wrap;
+}
+</style>
