850f8de4766c34712f6c36a219f9c2d0a8aac07b
hilmizul <netspytux@gmail.com>
2026-04-12 14:44:26 +0700

    small text nav

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -11,8 +11,8 @@
       </h6>
       <div class="col-md-3">
         <div class="card bg-shadow border-0 mb-4">
-          <div class="card-body">
-            <ul class="list-group">
+          <div class="card-body p-2">
+            <ul class="list-group small">
               <nuxt-link to="/admin" class="list-link border-bottom border-1">
                 <li class="list-group-item">
                   Statistik <span class="float-end"><i class="bi bi-chevron-right"></i></span>
@@ -28,9 +28,9 @@
                   Pengaturan <span class="float-end"><i class="bi bi-chevron-right"></i></span>
                 </li>
               </nuxt-link>
-              <nuxt-link to="/" class="list-link border-bottom border-1">
+              <nuxt-link to="/" target="_blank" class="list-link border-bottom border-1">
                 <li class="list-group-item">
-                  Ke Pengumuman <span class="float-end"><i class="bi bi-arrow-up-right-square"></i></span>
+                  Pengumuman <span class="float-end"><i class="bi bi-arrow-up-right-square"></i></span>
                 </li>
               </nuxt-link>
               <!-- <nuxt-link to="/" class="list-link border-bottom border-1">
