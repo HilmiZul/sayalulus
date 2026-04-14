41f1cf8c5475c6e965cba3f81907517982ea6a1b
hilmizul <netspytux@gmail.com>
2026-04-14 20:06:45 +0700

    small alert saved

--- pages/admin/pengaturan.vue
+++ pages/admin/pengaturan.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="card-body">
       <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-12">
           <div v-if="setting.length < 1" class="text-muted my-3">
             <span class="spinner-border spinner-border-sm me-1"></span>
             Sedang memuat
@@ -33,62 +33,70 @@
 
             <div id="myTabContent" class="tab-content">
               <div class="tab-pane active show" id="pengaturan-umum" role="tabpanel">
-                <form @submit.prevent="updateSetting">
-                  <div v-if="statusSuccessUpdate" class="alert alert-success alert-dismissible my-3">
-                    <button @click="() => statusSuccessUpdate = false" class="btn-close small"></button>
-                    Pengaturan berhasil diperbaharui.</div>
-                  <div class="my-3">
-                    <label for="nama_sekolah">Nama Sekolah</label>
-                    <input v-model="setting.nama_sekolah" id="nama_sekolah" type="text" class="form-control" placeholder="Nama Sekolah" required />
+                <div class="row">
+                  <div class="col-md-6">
+                    <form @submit.prevent="updateSetting">
+                      <div v-if="statusSuccessUpdate" class="alert alert-success alert-dismissible my-3 small">
+                        <button @click="() => statusSuccessUpdate = false" class="btn-close small"></button>
+                        Pengaturan berhasil disimpan.</div>
+                      <div class="my-3">
+                        <label for="nama_sekolah">Nama Sekolah</label>
+                        <input v-model="setting.nama_sekolah" id="nama_sekolah" type="text" class="form-control" placeholder="Nama Sekolah" required />
+                      </div>
+                      <div class="mb-3">
+                        <label for="alamat_sekolah">Alamat Sekolah</label>
+                        <textarea v-model="setting.alamat_sekolah" id="alamat_sekolah" rows="4" class="form-control" placeholder="Alamat lengkap sekolah" required></textarea>
+                      </div>
+                      <button class="btn btn-dark" :disabled="isSaving">
+                        <span v-if="isSaving">Sedang menyimpan</span>
+                        <span velse>Simpan</span>
+                      </button>
+                    </form>
                   </div>
-                  <div class="mb-3">
-                    <label for="alamat_sekolah">Alamat Sekolah</label>
-                    <textarea v-model="setting.alamat_sekolah" id="alamat_sekolah" rows="4" class="form-control" placeholder="Alamat lengkap sekolah" required></textarea>
-                  </div>
-                  <button class="btn btn-dark" :disabled="isSaving">
-                    <span v-if="isSaving">Sedang menyimpan</span>
-                    <span velse>Simpan</span>
-                  </button>
-                </form>
+                </div>
               </div>
 
               <div class="tab-pane" id="pengaturan-kelulusan" role="tabpanel">
-                <form @submit.prevent="updateSetting">
-                  <div v-if="statusSuccessUpdate" class="alert alert-success alert-dismissible my-3">
-                    <button @click="() => statusSuccessUpdate = false" class="btn-close small"></button>
-                    Pengaturan berhasil diperbaharui.</div>
-                  <div class="my-3">
-                    <label for="nomor">Nomor SK</label>
-                    <input v-model="setting.nomor_surat" id="nomor" type="text" class="form-control" placeholder="-/-/SMKN.4-Cabdin Wil.XII" required />
+                <div class="row">
+                  <div class="col-md-6">
+                    <form @submit.prevent="updateSetting">
+                      <div v-if="statusSuccessUpdate" class="alert alert-success alert-dismissible my-3 small">
+                        <button @click="() => statusSuccessUpdate = false" class="btn-close small"></button>
+                        Pengaturan berhasil disimpan.</div>
+                      <div class="my-3">
+                        <label for="nomor">Nomor SK</label>
+                        <input v-model="setting.nomor_surat" id="nomor" type="text" class="form-control" placeholder="-/-/SMKN.4-Cabdin Wil.XII" required />
+                      </div>
+                      <div class="mb-3">
+                        <label for="tgl_penetapan">Tanggal Penetapan</label>
+                        <input v-model="setting.tgl_penetapan" id="tgl_penetapan" type="date" class="form-control" placeholder="Tanggal penetapan" required />
+                      </div>
+                      <hr class="my-4 border-dark" />
+                      <div class="mb-3">
+                        <label for="tgl_pengumuman">Tanggal Pengumuman</label>
+                        <input v-model="setting.tgl_pengumuman" id="tgl_pengumuman" type="date" class="form-control" placeholder="Tanggal pengumuman" required />
+                      </div>
+                      <div class="mb-3">
+                        <label for="waktu_pengumuman">Waktu Pengumuman</label>
+                        <input v-model="setting.waktu_pengumuman" id="waktu_pengumuman" type="time" class="form-control" placeholder="Waktu pengumuman" required />
+                      </div>
+                      <button class="btn btn-dark" :disabled="isSaving">
+                        <span v-if="isSaving">Sedang menyimpan</span>
+                        <span velse>Simpan</span>
+                      </button>
+                    </form>
                   </div>
-                  <div class="mb-3">
-                    <label for="tgl_penetapan">Tanggal Penetapan</label>
-                    <input v-model="setting.tgl_penetapan" id="tgl_penetapan" type="date" class="form-control" placeholder="Tanggal penetapan" required />
-                  </div>
-                  <hr class="my-4 border-dark" />
-                  <div class="mb-3">
-                    <label for="tgl_pengumuman">Tanggal Pengumuman</label>
-                    <input v-model="setting.tgl_pengumuman" id="tgl_pengumuman" type="date" class="form-control" placeholder="Tanggal pengumuman" required />
-                  </div>
-                  <div class="mb-3">
-                    <label for="waktu_pengumuman">Waktu Pengumuman</label>
-                    <input v-model="setting.waktu_pengumuman" id="waktu_pengumuman" type="time" class="form-control" placeholder="Waktu pengumuman" required />
-                  </div>
-                  <button class="btn btn-dark" :disabled="isSaving">
-                    <span v-if="isSaving">Sedang menyimpan</span>
-                    <span velse>Simpan</span>
-                  </button>
-                </form>
+                </div>
               </div>
 
               <div class="tab-pane" id="pengaturan-pernyataan" role="tabpanel">
                 <form @submit.prevent="updateSetting">
-                  <div v-if="statusSuccessUpdate" class="alert alert-success alert-dismissible my-3">
+                  <div v-if="statusSuccessUpdate" class="alert alert-success alert-dismissible my-3 small">
                     <button @click="() => statusSuccessUpdate = false" class="btn-close small"></button>
-                    Pengaturan berhasil diperbaharui.</div>
+                    Pengaturan berhasil disimpan.</div>
                   <div class="my-3">
                     <label for="pernyataan">Pernyataan Siswa</label>
-                    <textarea v-model="setting.pernyataan_siswa" id="pernyataan" rows="6" class="form-control" placeholder="Tulis teks pernyataan siswa sebelum memeriksa hasil kelulusan..." required></textarea>
+                    <textarea v-model="setting.pernyataan_siswa" id="pernyataan" rows="8" class="form-control" placeholder="Tulis teks pernyataan siswa sebelum memeriksa hasil kelulusan..." required></textarea>
                   </div>
                   <button class="btn btn-dark" :disabled="isSaving">
                     <span v-if="isSaving">Sedang menyimpan</span>
